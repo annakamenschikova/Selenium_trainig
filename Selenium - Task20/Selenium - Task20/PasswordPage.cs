15bf7d55fce3f673b11611a2807d1c4feb66e0d1
Anna Kamenschikova <annitra.w@gmail.com>
2022-07-18 16:04:39 +0300

    First Commit of Task20

---
+++ Selenium - Task20/Selenium - Task20/PasswordPage.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Selenium___Task20
+{
+    public class PasswordPage
+    {
+        IWebDriver _driver;
+
+        const string PasswordFieldLocator = "passwd";
+        const string EnterButtonLocator = "//button[@id=\"passp:sign-in\"]";
+        public IWebElement PasswordField { get; set; }
+        public IWebElement EnterButton { get; set; }
+        public PasswordPage(IWebDriver driver)
+        {
+            _driver = driver;
+            PasswordField = _driver.FindElement(By.Name(PasswordFieldLocator));
+            EnterButton = _driver.FindElement(By.XPath(EnterButtonLocator));
+        }
+        public void EnterPassword(string password)
+        {
+            PasswordField.Click();
+            PasswordField.SendKeys(password);
+            EnterButton.Click();
+        }
+    }
+}
---
+++ Selenium - Task20/Selenium - Task20/UsernamePage.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Selenium___Task20
+{
+    public class UsernamePage
+    {
+        IWebDriver _driver;
+        const string EnterUsernameButtonLocator = "//button[@id = 'passp:sign-in']";
+        const string UsernameFieldLocator = "passp-field-login";
+
+        public IWebElement UsernameField { get; set; }
+        public IWebElement EnterUsernameButton { get; set; }
+
+
+        public UsernamePage(IWebDriver driver)
+        {
+            _driver = driver;
+            UsernameField = _driver.FindElement(By.Id(UsernameFieldLocator));
+            EnterUsernameButton = _driver.FindElement(By.XPath(EnterUsernameButtonLocator));
+
+
+        }
+
+        public void EnterUsername(string username)
+        {
+            UsernameField.Click();
+            UsernameField.SendKeys(username);
+            EnterUsernameButton.Click();
+        }
+    }
+}
